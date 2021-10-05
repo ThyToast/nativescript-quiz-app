d27d45270e78f81657635c4e2f2b6b02907f1cc9
Lee <hangsen.lee@hexainitiative.com>
2021-10-06 00:22:44 +0800

    added quiz page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
-import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core'
-import { NativeScriptModule } from '@nativescript/angular'
+import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
+import {
+  NativeScriptHttpClientModule,
+  NativeScriptModule,
+} from "@nativescript/angular";
 
-import { AppRoutingModule } from './app-routing.module'
-import { AppComponent } from './app.component'
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
 
 @NgModule({
   bootstrap: [AppComponent],
-  imports: [NativeScriptModule, AppRoutingModule],
+  imports: [NativeScriptModule, AppRoutingModule, NativeScriptHttpClientModule],
   declarations: [AppComponent],
   schemas: [NO_ERRORS_SCHEMA],
 })
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,109 @@
-import { Component, OnInit } from '@angular/core'
+import { QuizApiService } from "./../quiz-api.service";
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { RouterExtensions } from "@nativescript/angular";
 
 @Component({
-  selector: 'Home',
-  templateUrl: './home.component.html',
+  selector: "Home",
+  templateUrl: "./home.component.html",
 })
 export class HomeComponent implements OnInit {
-  constructor() {
-    // Use the component constructor to inject providers.
+  items: string[];
+  difficulty: string[];
+  number: number[];
+  category: any[];
+  categoryId: string[];
+  categoryNameObs: any[];
+  questionTypeObs: any[];
+
+  selectedQuestionNo: number;
+  selectedCategory: string;
+  selectedDifficulty: string;
+  selectedQuestionType: string;
+
+  constructor(
+    private quizApi: QuizApiService,
+    private router: RouterExtensions
+  ) {
+    this.items = ["kek", "lmao"];
+    this.number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    this.difficulty = this.quizApi.difficulty;
   }
 
   ngOnInit(): void {
-    // Init your component properties here.
+    this.getCategory();
+    this.getQuestionType();
+  }
+
+  getCategory() {
+    let categoryName = [];
+    let categoryId = [];
+    this.quizApi.getCategory().subscribe((category: any) => {
+      this.category = category.trivia_categories;
+      this.category.map((category) => {
+        categoryName.push(category.name);
+        categoryId.push(category.id);
+      });
+      this.categoryNameObs = categoryName;
+      this.categoryId = categoryId;
+    });
+  }
+
+  getQuestionType() {
+    let questionType = [];
+    this.quizApi.questionType.map((type) => {
+      let i = 0;
+      questionType.push(type[i]);
+      i++;
+    });
+    this.questionTypeObs = questionType;
+  }
+
+  createQuiz() {
+    this.quizApi
+      .getQuiz(
+        this.selectedQuestionNo,
+        this.selectedCategory,
+        this.selectedDifficulty,
+        this.selectedQuestionType
+      )
+      .subscribe((quiz) => {
+        this.router.navigate(["/quiz"], {
+          state: {
+            quiz,
+          },
+        });
+      });
+  }
+
+  getSelectedValue(event: any, type: string) {
+    switch (type) {
+      case "number": {
+        let selectedQuestionNo = event + 1;
+        console.log("selected question number: ", selectedQuestionNo);
+        this.selectedQuestionNo = selectedQuestionNo;
+        break;
+      }
+      case "difficulty": {
+        this.selectedDifficulty = this.difficulty[event].toLowerCase();
+        console.log("selected difficulty: ", this.selectedDifficulty);
+
+        break;
+      }
+      case "type": {
+        this.selectedQuestionType = this.quizApi.questionType[event][1];
+        console.log("selected type: ", this.selectedQuestionType);
+        break;
+      }
+      case "category": {
+        if (this.categoryId) {
+          this.selectedCategory = this.categoryId[event];
+          console.log("selected category: ", this.selectedCategory);
+          break;
+        }
+      }
+
+      default: {
+      }
+    }
   }
 }
