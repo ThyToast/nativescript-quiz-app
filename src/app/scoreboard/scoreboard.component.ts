cf4938eac2159ebcd585336420d3cc243ee415a3
Lee <hangsen.lee@hexainitiative.com>
2021-10-07 17:55:28 +0800

    display scoreboard data

--- src/app/scoreboard/scoreboard.component.ts
+++ src/app/scoreboard/scoreboard.component.ts
@@ -1,13 +1,16 @@
+import { quizResult } from "./../result/result.model";
 import { Component, OnInit } from "@angular/core";
+import { getString } from "@nativescript/core/application-settings";
 
 @Component({
   selector: "app-scoreboard",
   templateUrl: "./scoreboard.component.html",
 })
 export class ScoreboardComponent implements OnInit {
-  constructor() {}
+  storedResult: quizResult[];
+  constructor() {
+    this.storedResult = JSON.parse(getString("quiz"));
+  }
 
   ngOnInit(): void {}
-
-  getResults() {}
 }
