04cc62d2a631f603d1c4626efbca81e411fe5304
Lee <hangsen.lee@hexainitiative.com>
2021-10-07 17:17:27 +0800

    created scoreboard page, added storage

---
+++ src/app/scoreboard/scoreboard.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-scoreboard",
+  templateUrl: "./scoreboard.component.html",
+})
+export class ScoreboardComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  getResults() {}
+}
